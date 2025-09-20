b36b646ac0396c76f7e8f3e5cb4037326a2deae6
Flafi123 <ubiwaka1@bk.ru>
2025-09-20 16:01:25 +0300

    add chek sellers

--- src/main.js
+++ src/main.js
@@ -17,7 +17,14 @@ function calculateSimpleRevenue(purchase, _product) {
 }
 
 function analyzeSalesData(data, options) {
-    if (!data || !data.sellers || !data.customers || !data.products || !data.purchase_records) {
+    
+    data.purchase_records.forEach(record => {
+        const seller = sellerIndex[record.seller_id];
+        if (!seller) {
+            throw new Error(`Продавец с ID ${record.seller_id} не найден`);
+        }
+    });
+    if (!data || !data.sellers || !data.customers || !data.products || !data.purchase_records ) {
         throw new Error('Некорректные входные данные');
     }
 
